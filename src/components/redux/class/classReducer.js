d7604f14e370efb73c9d9be684ef7c19d4233273
rajasai <rajasaidevaraju@yahoo.com>
2020-09-04 13:00:50 +0530

    minor changes in classReducer.js

--- src/components/redux/class/classReducer.js
+++ src/components/redux/class/classReducer.js
@@ -1,33 +1,39 @@
-import { ADD_CLASS, DELETE_CLASS, SET_CLASSES } from "./classActionTypes";
+import {
+  ADD_CLASS,
+  REMOVE_CLASS,
+  SET_CLASSES,
+  SET_CLASS_ERROR,
+} from "./classActionTypes";
 
-const initialState={
-    classes=[],
-    error={}
-}
+const initialState = {
+  classes: [],
+  error: {},
+};
 
-const classReducer=(state = initialState,action)=>{
-    switch (action.type) {
-        
-        case ADD_CLASS:
-            state.classes.push(action.payload)
-            return state;
-            
-        case DELETE_CLASS:
-            const del_index=action.payload;
-            const newArray=state.classes.filter((value, index)=>{return index!=del_index});
-            state.classes=newArray;
-            return state;
-            
-        case SET_CLASSES:
-            state.classes=action.payload;
-            return state;
+const classReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_CLASS:
+      state.classes.push(action.payload);
+      return state;
 
-        case SET_CLASS_ERROR:
-            state.error=action.payload;
-            return state;
-        default:
-            return state;
-    }
-}
+    case REMOVE_CLASS:
+      const del_index = action.payload;
+      const newArray = state.classes.filter((value, index) => {
+        return index != del_index;
+      });
+      state.classes = newArray;
+      return state;
+
+    case SET_CLASSES:
+      state.classes = action.payload;
+      return state;
+
+    case SET_CLASS_ERROR:
+      state.error = action.payload;
+      return state;
+    default:
+      return state;
+  }
+};
 
 export default classReducer;
